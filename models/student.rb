1de7bcfef746ef2098d14e3b55d0d627489f9b6d
Isiah VanBrackle <squarepheonix@gmail.com>
2018-08-07 15:11:31 -0400

    Done.

---
+++ models/student.rb
@@ -0,0 +1,15 @@
+class Student
+  attr_reader :name, :grade
+
+  @@students = []
+
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    @@students << self
+  end
+
+  def self.all
+    @@students
+  end
+end
